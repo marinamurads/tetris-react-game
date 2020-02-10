41833166bc1f870cf9f9164d18aa1e8b7d05efd2
marinamurads <marinamuradgs@gmail.com>
2020-02-10 12:34:19 -0300

    Updates

---
+++ precache-manifest.6ed9f489e5c2189ffea2f59b12c18244.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aaba8888c1f8acd13895be72ff5da730",
+    "url": "/tetris-react-game/index.html"
+  },
+  {
+    "revision": "03b6a0b69e2c71631a7d",
+    "url": "/tetris-react-game/static/css/main.805ccc92.chunk.css"
+  },
+  {
+    "revision": "119663c88f914a32cbdd",
+    "url": "/tetris-react-game/static/js/2.2896dd9a.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/tetris-react-game/static/js/2.2896dd9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03b6a0b69e2c71631a7d",
+    "url": "/tetris-react-game/static/js/main.8bbe9148.chunk.js"
+  },
+  {
+    "revision": "e28a659dfdf1b114bbfd",
+    "url": "/tetris-react-game/static/js/runtime-main.f4d056a0.js"
+  },
+  {
+    "revision": "08aa6c8eba9461f69628d14ea9a200b8",
+    "url": "/tetris-react-game/static/media/34805.08aa6c8e.jpg"
+  },
+  {
+    "revision": "e28975521df42a470a60593872337098",
+    "url": "/tetris-react-game/static/media/Pixel-LCD-7.e2897552.woff"
+  }
+]);
